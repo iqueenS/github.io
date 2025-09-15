1b63646e5c8a049320551cad39d663039f87ff39
iqueenS <ireminaj@gmail.com>
2025-09-15 23:07:17 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,1 @@
-document.getElementById('year').textContent = new Date().getFullYear();
-const burger = document.getElementById('burger');
-const nav = document.getElementById('navlinks');
-burger.addEventListener('click',()=> nav.classList.toggle('open'));
+
